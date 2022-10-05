e1635ba2bc6bcaca238701ebebd27161f0410bea
Aritrajana-eng <66968516+Aritrajana-eng@users.noreply.github.com>
2022-10-06 00:31:55 +0530

    Add files via upload

---
+++ migrations/20220929190557-create-user-education.js
@@ -0,0 +1,46 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('user_educations', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "USER_CREDENTIALs",
+          key: 'id'
+        },
+        onDelete: 'CASCADE'
+      },
+      from_year: {
+        type: Sequelize.DATE
+      },
+      degree_name: {
+        type: Sequelize.STRING
+      },
+      to_year: {
+        type: Sequelize.DATE
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      deletedAt: {
+        allowNull: true,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('user_educations');
+  }
+};
